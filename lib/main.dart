4e9ac9d91921902d126f9c783b83748fe8a0aae7
Nitish Churiwala <churiwalanitish2003@gmail.com>
2023-08-27 12:55:16 +0530

    Completed Firebase Setup.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'track_truck_screen.dart';
 // import 'special_booking_screen.dart';
 import 'profile_screen.dart';
 import 'manage_booking_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
--- lib/track_truck_screen.dart
+++ lib/track_truck_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
 class TrackTruckScreen extends StatelessWidget {
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(12.93626232871436, 77.60621561694676),
@@ -10,9 +11,16 @@ class TrackTruckScreen extends StatelessWidget {
   final Set<Marker> _markers = {
     const Marker(
       markerId: MarkerId('marker_1'),
+      position: LatLng(12.932970143527706, 77.61036443809877),
+      infoWindow: InfoWindow(
+        title: 'My Location 2',
+      ),
+    ),
+    const Marker(
+      markerId: MarkerId('marker_2'),
       position: LatLng(12.93626232871436, 77.60621561694676),
       infoWindow: InfoWindow(
-        title: 'Your Current Location',
+        title: 'My Location',
       ),
     ),
   };
@@ -20,10 +28,16 @@ class TrackTruckScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        markers: _markers,
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: _markers,
+        ),
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.location_disabled_outlined),
+      //   onPressed: ,
+      // ),
     );
   }
 }
